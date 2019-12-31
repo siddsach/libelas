1f35e1f2338334bc0de3d3fe923a9dac35125e6e
Siddharth Sachdeva <siddsach@gmail.com>
2019-12-31 17:17:54 +0800

    First commit

---
+++ CMakeFiles/elas.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/siddharthsachdeva/repos/libelas/src/descriptor.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/descriptor.cpp.o"
+  "/Users/siddharthsachdeva/repos/libelas/src/elas.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/elas.cpp.o"
+  "/Users/siddharthsachdeva/repos/libelas/src/filter.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/filter.cpp.o"
+  "/Users/siddharthsachdeva/repos/libelas/src/main.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/main.cpp.o"
+  "/Users/siddharthsachdeva/repos/libelas/src/matrix.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/matrix.cpp.o"
+  "/Users/siddharthsachdeva/repos/libelas/src/triangle.cpp" "/Users/siddharthsachdeva/repos/libelas/CMakeFiles/elas.dir/src/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
